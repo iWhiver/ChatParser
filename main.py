da3a289e3ad87d37dce15db773339ae1186bbad1
Belorechev Ivan <tencet@yandex.ru>
2022-12-27 17:18:11 +0800

    Used changes from tsyganno branch

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
-import asyncio
 import uvloop
 import asyncio
 import configparser
 from json import loads
 from pyrogram import Client, errors, enums
 
-config = configparser.ConfigParser()  # создаём объекта парсера
-config.read(".ini")  # читаем конфиг
+config = configparser.ConfigParser()
+config.read(".ini")
 
 api_id = config['Config']['API_ID']
 api_hash = config['Config']['API_HASH']
@@ -18,8 +17,6 @@ chats = [
     'terncrypto_otc',
     'MarketICOBOG'
 ]
-
-# doubletop - чат, который банит виртуальные аккаунты
 
 MESSAGE_LIMIT = 50
 SEARCH_COIN = 'Binance'.lower()
@@ -36,23 +33,20 @@ async def main():
         await logic(app)
 
 
+def create_a_message(dict_message, text):
+    html_id_user = f"t.me/user?id={dict_message['from_user']['id']}"
+    message = f"Chat: {dict_message['chat']['title']}\n" \
+              f"Id user: {html_id_user}\n" \
+              f"Username: {dict_message['from_user']['username']}\n" \
+              f"Text: {text}"
+    return message
+
+
 async def logic(app: Client) -> None:
     try:
         for chat in chats:
             async for message in app.get_chat_history(chat, limit=MESSAGE_LIMIT):
                 dict_message = loads(str(message))
-                title_chat = dict_message['chat']['title']
-                id_user = int(dict_message['from_user']['id'])
-                first_name = 'None'
-                if 'first_name' in dict_message['from_user']:
-                    first_name = dict_message['from_user']['first_name']
-                last_name = 'None'
-                if 'last_name' in dict_message['from_user']:
-                    last_name = dict_message['from_user']['last_name']
-                username = 'None'
-                if 'username' in dict_message['from_user']:
-                    username = dict_message['from_user']['username']
-
                 # date = dict_message['date']
                 text = dict_message['text'].lower()
                 if ((('wts' in text and WTS) and SEARCH_COIN in text) or
@@ -61,15 +55,7 @@ async def logic(app: Client) -> None:
                     if text not in data:
                         data.append(text)
 
-                    # html_id_user = f'<a href="tg://user?id={id_user}">{id_user}</a>'
-                    # message = f'Chat: {title_chat}\n' \
-                    #           f'Id user: {html_id_user}\n' \
-                    #           f'First name: {first_name}\n' \
-                    #           f'Last name: {last_name}\n' \
-                    #           f'Username: {username}\n' \
-                    #           f'Text: {text}'
-
-                    message = text
+                    message = create_a_message(dict_message, text)
                     print(message)
                     print("=========================================")
                     await app.send_message("me", message, parse_mode=enums.ParseMode.HTML)
@@ -79,7 +65,7 @@ async def logic(app: Client) -> None:
         print('wait after flood')
         await asyncio.sleep(wait_err.value)
 
-    await asyncio.sleep(60)
+    # await asyncio.sleep(60)
 
 
 uvloop.install()
