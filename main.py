0fe02670cd13c89fb7cb5a170d2ca46a72b73174
Belorechev Ivan <tencet@yandex.ru>
2023-01-15 11:02:14 +0800

    Clear files

--- main.py
+++ main.py
@@ -26,8 +26,13 @@ data = []
 
 
 async def main():
-    app = Client("my_account", api_id=api_id, api_hash=api_hash,
-                 phone_number=phone_number, password=password)
+    app = Client(
+        "my_account",
+        api_id=api_id,
+        api_hash=api_hash,
+        phone_number=phone_number,
+        password=password
+    )
 
     async with app:
         await logic(app)
@@ -47,26 +52,25 @@ async def logic(app: Client) -> None:
         for chat in chats:
             async for message in app.get_chat_history(chat, limit=MESSAGE_LIMIT):
                 dict_message = loads(str(message))
-                # date = dict_message['date']
                 text = dict_message['text'].lower()
                 if ((('wts' in text and WTS) and SEARCH_COIN in text) or
                         ('wtb' in text and WTB) and SEARCH_COIN in text):
 
-                    if text not in data:
-                        data.append(text)
+                    if text not in uniq_text:
+                        uniq_text.append(text)
 
                     message = create_a_message(dict_message, text)
                     print(message)
                     print("=========================================")
-                    await app.send_message("me", message, parse_mode=enums.ParseMode.HTML)
-            await asyncio.sleep(10)
+                    if SEND_MESSAGE_TO_ME:
+                        await app.send_message("me", message, parse_mode=enums.ParseMode.HTML)
+        await asyncio.sleep(10)
 
     except errors.exceptions.flood_420.FloodWait as wait_err:
         print('wait after flood')
         await asyncio.sleep(wait_err.value)
-
-    # await asyncio.sleep(60)
-
+    except Exception as e:
+        print(e)
 
 uvloop.install()
 asyncio.run(main())
